f9c0ba1fbd2242ceedc0648dbc5f7c4cd730784a
sss <shahramsss>
2024-10-23 12:11:54 +0330

    add product price humanize int comma

--- accounting/accounting/settings.py
+++ accounting/accounting/settings.py
@@ -37,8 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # local apps
     'home',
     'account',
+
+    # third apps
+    'django.contrib.humanize',
     
 
 
